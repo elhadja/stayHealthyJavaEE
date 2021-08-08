61d496f89b1269b4deaf72bd38404706afbd5c79
elhadja <elhadja007@gmail.com>
2021-08-08 22:41:13 +0200

    Initial Backend version

---
+++ PMBackend/src/test/java/com/elhadjium/PMBackend/PmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elhadjium.PMBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
