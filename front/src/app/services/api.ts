2eeb3ce1f0a3d6c2a237a7da0e612ad60deb29b7
Elhadj amadou BAH <elhadja007@gmail.com>
2022-11-23 22:58:07 +0100

    complete environnement variables

--- front/src/app/services/api.ts
+++ front/src/app/services/api.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http
 import { Injectable } from "@angular/core";
 import { Observable, ObservableInput, throwError } from "rxjs";
 import { catchError } from 'rxjs/operators'
+import { environment } from "src/environments/environment";
 import { MessageService } from "./Message.service";
 
 @Injectable()
@@ -12,7 +13,7 @@ export class API {
     };
 
     constructor(private httpClient: HttpClient, private messageService: MessageService) {
-        this.baseURI = "http://localhost:8080";
+        this.baseURI = environment.api_base_uri;
         this.httpOptions = {
             headers: new HttpHeaders({
                 'Content-Type': 'application/json',
